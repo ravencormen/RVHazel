3124e11571ae86b08d2e272a7b8f0ef2c4da9e64
ravencormen <neargray@gmail.com>
2021-12-16 15:14:16 +0800

    20211216

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,49 @@
+#include <Hazel.h>
+
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+			HZ_TRACE("Tab key is pressed (poll)!");
+	}
+
+	void OnEvent(Hazel::Event& event) override
+	{
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == HZ_KEY_TAB)
+				HZ_TRACE("Tab key is pressed (event)!");
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
+	}
+
+};
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+		PushLayer(new ExampleLayer());
+		PushOverlay(new Hazel::ImGuiLayer());
+	}
+
+	~Sandbox()
+	{
+
+	}
+
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
